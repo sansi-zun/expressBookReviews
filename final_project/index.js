0a284c22c53ca45266c60f549fd4b4f106e59e06
Some Name <ralf.holger@keemail.me>
2023-03-11 17:40:36 -0500

    minor cleanup

--- final_project/index.js
+++ final_project/index.js
@@ -5,18 +5,21 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+const PORT = 5000;
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({
+    secret: "fingerprint_customer", 
+    resave: true, 
+    saveUninitialized: true
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
+app.use("/customer/auth/*", function auth(req, res, next){
 //Write the authenication mechanism here
 });
  
-const PORT =5000;
-
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
