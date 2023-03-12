34cbcf446bb8d021b6d3a87e01eaf6abe4d4ae4e
Some Name <ralf.holger@keemail.me>
2023-03-11 21:10:26 -0500

    finished final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,19 +10,27 @@ public_users.post("/register", (req, res) => {
     const password = req.body.password;
 
     if (!username || !password) {
-        return res.status(406).json({message: "unable to register user"});
+        return res.status(406).json({ message: "unable to register user" });
     }
     else if (isValid(username)) {
-        return res.status(406).json({message: "user is already registered"});
+        return res.status(406).json({ message: "user is already registered" });
     }
 
-    users.push({username, password});
+    users.push({ username, password });
     res.send({ message: `user ${username} successfully registered` });
 });
 
 // Get the book list available in the shop
 public_users.get('/', (req, res) => {
     res.send(JSON.stringify({ books }, null, 4));
+});
+
+// TASK 10 - Get the book list available in the shop using promises
+public_users.get('/async', (req, res) => {
+    const prom = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({ books }, null, 4)));
+    });
+    prom.then(() => console.log("Promise for Task 10 resolved"));
 });
 
 // Get book details based on ISBN
@@ -33,6 +41,21 @@ public_users.get('/isbn/:isbn', (req, res) => {
         return res.status(404).json({ message: `no book with isbn ${isbn}` });
     }
     res.send(book);
+});
+
+// TASK 11 - Get book details based on ISBN using promises
+public_users.get('/async/isbn/:isbn', (req, res) => {
+    const prom = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        const book = books[isbn];
+        if (!book) {
+            reject(res.status(404).json({ message: `no book with isbn ${isbn}` }));
+        }
+        else {
+            resolve(res.send(book));
+        }
+    });
+    prom.then(() => console.log("Promise for Task 11 resolved"));
 });
 
 // Get book details based on author
@@ -47,6 +70,23 @@ public_users.get('/author/:author', (req, res) => {
     res.send(foundBooks);
 });
 
+// TASK 12 - Get book details based on author using promises
+public_users.get('/async/author/:author', (req, res) => {
+    const prom = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        const foundBooks = Object.keys(books)
+            .map(x => books[x])
+            .filter(x => x.author === author);
+        if (foundBooks.length === 0) {
+            reject(res.status(404).json({ message: `no books with author ${author}` }));
+        }
+        else {
+            resolve(res.send(foundBooks));
+        }
+    });
+    prom.then(() => console.log("Promise for Task 12 resolved"));
+});
+
 // Get all books based on title
 public_users.get('/title/:title', (req, res) => {
     const title = req.params.title;
@@ -57,6 +97,23 @@ public_users.get('/title/:title', (req, res) => {
         return res.status(404).json({ message: `no books with title ${title}` });
     }
     res.send(foundBooks);
+});
+
+// TASK 13 - Get all books based on title using promises
+public_users.get('/async/title/:title', (req, res) => {
+    const prom = new Promise((resolve, reject) => {
+        const title = req.params.title;
+        const foundBooks = Object.keys(books)
+            .map(x => books[x])
+            .filter(x => x.title === title);
+        if (foundBooks.length === 0) {
+            reject(res.status(404).json({ message: `no books with title ${title}` }));
+        }
+        else {
+            resolve(res.send(foundBooks));
+        }
+    });
+    prom.then(() => console.log("Promise for Task 13 resolved"));
 });
 
 //  Get book review
